d350dfd4e554d36db355d85cd9b25e8e1f04d5cc
Nadan Gergeo <nadan@trustly.com>
2014-03-30 03:31:02 +0200

    Fixed a little bug with addComment :)

--- js/fairy.js
+++ js/fairy.js
@@ -1,17 +1,21 @@
-angular.module("fairyApp", ["firebase"]).controller ("testController", ["$scope", "$firebase", 
+angular.module("fairyApp", ["firebase"]).controller("testController", ["$scope", "$firebase", 
     function($scope, $firebase) {
         var ref = new Firebase("https://scorching-fire-7581.firebaseIO.com/");
         $scope.comments = $firebase(ref);
         $scope.username = 'Guest' + Math.floor(Math.random() * 101);
         
         $scope.addComment = function(e) {
-            if(e.keyCode != 13) return;
+            if(e.keyCode != 13) {
+                return;
+            }
+            
             $scope.comments.$add({
                 from: $scope.username,
                 body: $scope.newComment
             });
             
             $scope.newComment = "";
+            e.preventDefault();
         }
     }
 ]);
