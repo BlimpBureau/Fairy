d79847efc5e05f2f727a4d929cebed178ad145d6
Nadan Gergeo <nadan@trustly.com>
2014-03-30 04:27:17 +0200

    Removed nav button for now

--- index.php
+++ index.php
@@ -27,12 +27,6 @@
       <div class="fairy-navbar navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
             <a class="navbar-brand" href="#"><img id="fairy-logo" src="img/fairy-logo-white-rgb.svg"></a>
           </div>
         </div>
