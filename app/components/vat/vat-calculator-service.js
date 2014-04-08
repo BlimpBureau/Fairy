f468fb4e774348f79a7cb8c5eaa9f9015b3f7092
Lucas Wiener <lucas@wiener.se>
2014-04-08 09:09:50 +0200

    Added more calculations to vat component and tests

--- app/components/vat/vat-calculator-service.js
+++ app/components/vat/vat-calculator-service.js
@@ -3,11 +3,27 @@
 angular.module('vat.calculator', [])
   .factory('vatCalculator', function() {
     return {
-      vatOfPrice: function(price, vatRate, priceIncludingVat) {
-        if(priceIncludingVat) {
+      vatOfPrice: function(price, vatRate, piv) {
+        if(piv) {
           return price - (price / (1 + vatRate));
         } else {
           return price * vatRate;
+        }
+      },
+
+      vatRateOfPrice: function(price, vat, piv) {
+        if(piv) {
+          return vat / (price - vat);
+        } else {
+          return vat / price;
+        }
+      },
+
+      priceOfVat: function(vat, vatRate, piv) {
+        if(piv) {
+          return (vat / vatRate) + vat;
+        } else {
+          return vat / vatRate;
         }
       }
     };
--- app/components/vat/vat-calculator-service_test.js
+++ app/components/vat/vat-calculator-service_test.js
@@ -22,12 +22,41 @@ describe('vat.calculator', function() {
       expect(vatCalculator.vatOfPrice(100, 0.25)).toEqual(25);
     });
 
-    it('should work with float price values', function() {
+    it('should work with float vat values', function() {
       expect(vatCalculator.vatOfPrice(1, 0.1)).toEqual(0.1);
     });
 
     it('should return the correct vat by price inclusive vat', function() {
       expect(vatCalculator.vatOfPrice(100, 0.25, true)).toEqual(20);
+    });
+  });
+
+  describe('vatRateOfPrice', function() {
+    it('should return the correct vat rate by price exlcusive vat', function() {
+      expect(vatCalculator.vatRateOfPrice(100, 25, false)).toEqual(0.25);
+    });
+
+    it('should default priceIncludingVat parameter to false', function() {
+      expect(vatCalculator.vatRateOfPrice(100, 10)).toEqual(0.1);
+    });
+
+    it('should return the correct vat rate by price inclusive vat', function() {
+      expect(vatCalculator.vatRateOfPrice(100, 20, true)).toEqual(0.25);
+    });
+  });
+
+  describe('priceOfVat', function() {
+    it('should return the correct price by price exclusive vat', function() {
+      expect(vatCalculator.priceOfVat(25, 0.25, false)).toEqual(100);
+    });
+
+    it('should default priceIncludingVat parameter to false', function() {
+      expect(vatCalculator.priceOfVat(10, 0.1)).toEqual(100);
+    });
+
+    it('should return the correct price by price inclusive vat', function() {
+      expect(vatCalculator.priceOfVat(20, 0.25, true)).toEqual(100);
+      expect(vatCalculator.priceOfVat(10, 0.1, true)).toEqual(110);
     });
   });
 });
