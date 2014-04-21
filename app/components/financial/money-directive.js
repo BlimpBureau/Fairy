f50eaae119dd03273e6574b6b5236b3cd0def601
Lucas Wiener <lucas@wiener.se>
2014-04-21 22:28:18 +0200

    Fixed money directive so it makes the model a number. Removed money directive from vatrate

--- app/components/financial/money-directive.js
+++ app/components/financial/money-directive.js
@@ -8,7 +8,23 @@ angular.module('financial.money', [])
     link: function(scope, element, attr, controller) {
       var attrDecimals = parseInt(attr.decimals);
 
+      var MONEY_REGEXP = /^\d+((.|,)\d+)?$/;
+      
       var NUM_DECIMALS = isNaN(attrDecimals) ? 2 : attrDecimals;
+      
+      controller.$parsers.push(function(viewValue) {
+        if(viewValue) {
+          if(MONEY_REGEXP.test(viewValue)) {
+            controller.$setValidity('vatRate', true);
+
+            //Make sure any comma is converted to a dot.
+            return parseFloat(viewValue.replace(',', '.'));
+          } else {
+            controller.$setValidity('vatRate', false);
+            return undefined;
+          }
+        }
+      });
 
       controller.$formatters.unshift(function(modelValue) {
         var number = parseFloat(modelValue);
