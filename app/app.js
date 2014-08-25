b5a99a409d22eb6f0e1b9b51643316b739b13865
Lucas Wiener <lucas@wiener.se>
2014-08-25 11:39:23 +0200

    Added decorator for ngModel and ngForm to support dyanmic name attributes

--- app/app.js
+++ app/app.js
@@ -31,4 +31,41 @@ angular.module("fairyApp", [
         redirectTo: "/dashboard"
     })
     ;
-}]);
+}])
+
+//Workaround for bug #1404.
+//This will allow ngModel and ngForm directives to have dynamic name attributes (expression evaluation in name).
+//https://github.com/angular/angular.js/issues/1404
+//Source: http://plnkr.co/edit/hSMzWC?p=preview
+.config(["$provide", function($provide) {
+    $provide.decorator("ngModelDirective", ["$delegate", function($delegate) {
+        var ngModel = $delegate[0];
+        var controller = ngModel.controller;
+        ngModel.controller = ["$scope", "$element", "$attrs", "$injector", function(scope, element, attrs, $injector) {
+            var $interpolate = $injector.get("$interpolate");
+            attrs.$set("name", $interpolate(attrs.name || "")(scope));
+            $injector.invoke(controller, this, {
+                "$scope": scope,
+                "$element": element,
+                "$attrs": attrs
+            });
+        }];
+        return $delegate;
+    }]);
+    $provide.decorator("formDirective", ["$delegate", function($delegate) {
+        var form = $delegate[0];
+        var controller = form.controller;
+        form.controller = ["$scope", "$element", "$attrs", "$injector", function(scope, element, attrs, $injector) {
+            var $interpolate = $injector.get("$interpolate");
+            attrs.$set("name", $interpolate(attrs.name || attrs.ngForm || "")(scope));
+            $injector.invoke(controller, this, {
+                "$scope": scope,
+                "$element": element,
+                "$attrs": attrs
+            });
+        }];
+        return $delegate;
+    }]);
+}])
+
+;
