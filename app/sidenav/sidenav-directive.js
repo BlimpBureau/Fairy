faaf183915a646b6964762650e403744eb90467c
Lucas Wiener <lucas@wiener.se>
2014-10-01 17:16:35 +0200

    Made navbar items determined by scope array of items dynamically

--- app/sidenav/sidenav-directive.js
+++ app/sidenav/sidenav-directive.js
@@ -8,7 +8,21 @@ angular.module("sidenav", [])
         restrict: "E",
         templateUrl: "sidenav/sidenav.html",
         link: function(scope, element, attr, controller) {
-
+            scope.items = [
+                item("#", "Lucas Wiener", "head"),
+                item("#", "Dashboard", "bar-graph-2"),
+                item("#", "Ledger", "book"),
+                item("#", "Settings", "cog"),
+                item("#/logout", "Sign out", "umbrella")
+            ];
         }
     };
 });
+
+function item(href, text, icon) {
+    return {
+        href: href,
+        text: text,
+        icon: icon
+    }
+}
